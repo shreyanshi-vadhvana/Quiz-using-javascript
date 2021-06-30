cc54a637779dfc48406f9d7003c052f929f78fec
shreyanshi-vadhvana <shreyanshi.dv3@gmail.com>
2021-06-30 15:04:33 +0530

    Erros resolved

--- quiz.js
+++ quiz.js
@@ -1,21 +1,11 @@
-(function(){
-        const Questions=[
+(function() {
+        const Questions = [
             {
-                question: "Name the Indian State banned 12 brands of pan masala for a period of 1 year?",
-                answers:{
-                a:"Uttar Pradesh",
-                b:"Bihar",
-                c:"Gujarat",
-                },
-                correctAnswer:"b"
-            },
-
-            {
-                question:"Entomology is the science that studies:",
-                answers:{
-                    a:"Insects",
-                    b:"The formation of rocks",
-                    c:"Behavior of human beings",
+                question: "What is anthropology? ",
+                answers: {
+                    a:"Study of humanity",
+                    b:"Study of plants",
+                    c:"Study of human behaviour",
                 },
                 correctAnswer:"a"
             },
@@ -28,6 +18,17 @@
                 },
                 correctAnswer:"c"
             },
+            {
+                question:"Entomology is the science that studies:",
+                answers:
+                {
+                    a:"Insects",
+                    b:"The formation of rocks",
+                    c:"Behavior of human beings",
+                },
+                correctAnswer:"a"
+            },
+            
             {
                 question:"Who is the President of India (as of 2021)?",
                 answers:{
@@ -48,18 +49,21 @@
             },
         ];
 
-        function buildQuiz(){
+        function buildQuiz() {
             const output=[];
-
+        
             Questions.forEach((currentQuestion, questionNo) => {
                 const answers= [];
+
+                 //Aaddd input
 
                 for(letter in currentQuestion.answers){
                     answers.push(
                         `<label id="${questionNo}${letter}" href="#">
-                            <input type="radio" name="question${questionNo}" value="${letter}" id="${question}"></input>               
+                           
+                            <input type="radio" name="question${questionNo}" value="${letter}">   
                             ${letter} :
-                            ${currentQuestion.answer[letter]}
+                            ${currentQuestion.answers[letter]}
                         </label>`
                     );
                 }
@@ -73,86 +77,119 @@
             
             });
 
-            container.innerHTML=output.join("");   
+            quizContainer.innerHTML=output.join("");   
         }
 
         function showResults(){
-            const answerContainers = container.querySelectorAll(".answers");
+            const answerContainers = quizContainer.querySelectorAll(".answers");
             let numCorrect=0;
-            
+             
             Questions.forEach((currentQuestion, questionNo) => {
-                const answerContainer =answerContainers[questionNo];
+                const answerContainer = answerContainers[questionNo];
                 const selector = `label input[name=question${questionNo}]:checked`;
                 const userAnswer= (answerContainer.querySelector(selector) || {}).value;
                 const answerID= (answerContainer.querySelector(selector) || {}).id;
-                const selector1= `label[id="${answerID}"]`;
+                const selector1 = `label[id="${answerID}"]`;
                 var answerElem = answerContainer.querySelector(selector1);
                 const selector2 = `label[id="${questionNo}${currentQuestion.correctAnswer}"]`;
                 var answerElem1 = answerContainer.querySelector(selector2);
 
+                console.log(selector)
+                console.log(selector1)
+                console.log(selector2)
+                console.log(answerElem)
+
                 if(userAnswer === currentQuestion.correctAnswer){
                     numCorrect++;
 
-                    answerElem.style.color = "green";
+                    console.log(answerElem)
+                    console.log(answerElem1)
+                    //answerContainers[questionNo].style.color = 'lightgreen';
+                    answerElem.style.background= "#70F85A";
                     answerElem.style.fontWeight = "800";
                 }
 
                 else{
-                    answerElem1.style.color="red";
+
+                    answerElem.style.color= "#70F85A";
+                    answerElem.style.background= "#FD2929";
+                    //answerContainers[questionNo].style.color = 'red';
                     answerElem1.style.fontWeight = "800" ;
                 }
 
             });
 
-            resultscontainer.innerHTML=`${numCorrect} / ${Questions.length}`;
+             /*
+
+            Questions.forEach( (currentQuestion, questionNo) => {
+
+                // find selected answer
+                const answerContainer = answerContainers[questionNo];
+                const selector = `input[name=question${questionNo}]:checked`;
+                const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+            
+                // if answer is correct
+                if(userAnswer === currentQuestion.correctAnswer){
+                  // add to the number of correct answers
+                  numCorrect++;
+            
+                  // color the answers green
+                  answerContainers[questionNo].style.color = 'lightgreen';
+                }
+                // if answer is wrong or blank
+                else{
+                  // color the answers red
+                  answerContainers[questionNo].style.color = 'red';
+                }
+              });
+              */
+
+            resultscontainer.innerHTML= `${numCorrect} /  ${Questions.length}`;
         }
 
-        function showSlide(n){
-            slides[currentSlide].classList.remove("active-slide");
-            slides[n].classList.add("active-slide");
-            currentSlide=n;
-
+        function showSlide(n) {
+            slides[currentSlide].classList.remove('active-slide');
+            slides[n].classList.add('active-slide');
+            currentSlide = n;
             if(currentSlide === 0){
-                previousButton.style.display="none";
+              previousButton.style.display = 'none';
             }
-            
             else{
-                previousButton.style.display="inline-block";
+              previousButton.style.display = 'inline-block';
             }
-
             if(currentSlide === slides.length-1){
-                nextbutton.style.display="none";
-                submitbutton.style.display="inline-block";
+              nextButton.style.display = 'none';
+              submitButton.style.display = 'inline-block';
             }
-
             else{
-                nextButton.style.display="inline-block";
-                submitbutton.style.display="none";
+              nextButton.style.display = 'inline-block';
+              submitButton.style.display = 'none';
             }
         }
 
         function showNextSlide(){
-            showNextSlide(currentSlide + 1);
+            showSlide(currentSlide + 1);
         }
 
         function showPreviousSlide(){
-            showNextSlide(currentSlide-1);
+            showSlide(currentSlide-1);
         }
 
-        const container= document.getElementById("quiz");
+        const quizContainer= document.getElementById('quiz');
         const resultscontainer = document.getElementById("results");
-        const submitbutton= document.getElementById("submit");
+        const submitButton= document.getElementById("submit");
 
         buildQuiz();
 
         const previousButton = document.getElementById("previous");
         const nextButton = document.getElementById("next");
-        const slides= document.getElementById("next");
+        const slides= document.querySelectorAll(".slide");
+
         let currentSlide= 0;
 
         showSlide(0);
 
-        submitbutton.addEventListener("click", showResults);
+        submitButton.addEventListener("click", showResults);
         previousButton.addEventListener("click", showPreviousSlide);
         nextButton.addEventListener("click", showNextSlide);
-    })();
+})();
